3388f91ce8066a6b071e35e1619df225c0eea869
Slience <2890554485@qq.com>
2022-04-08 08:36:40 +0800

    init commit

---
+++ src/com/edu/base/Test.java
@@ -0,0 +1,11 @@
+package com.edu.base;
+
+/**
+ * @author Zcy
+ * @version 1.0
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
